e21970d545199adfce4a200f91fc1f216be27a63
张明朝 <1104272319@qq.com>
2018-09-25 01:26:44 +0800

    header组件增加logo子组件

---
+++ src/common/header/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+    HeaderWrapper,
+    Logo
+} from './style';
+
+class Header extends Component {
+    render() {
+        return (
+            <HeaderWrapper>
+                < Logo href='/' />
+            </HeaderWrapper>
+        )
+    }
+}
+
+export default Header;
