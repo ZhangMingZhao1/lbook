cd3e9bcc6292d13be9a507656aab5f8bc13a44f2
张明朝 <1104272319@qq.com>
2018-09-26 02:13:35 +0800

    完善header，增加搜索框，增加按钮

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import {
     HeaderWrapper,
-    Logo
+    Logo,
+    Nav,
+    NavItem,
+    Addition,
+    Button,
+    NavSearch,
 } from './style';
 
 class Header extends Component {
@@ -9,7 +14,19 @@ class Header extends Component {
         return (
             <HeaderWrapper>
                 < Logo href='/' />
+                <Nav>
+                    <NavItem className="left active">首页</NavItem>
+                    <NavItem className="left">下载APP</NavItem>
+                    <NavItem className="right">登陆</NavItem>
+                    <NavItem className="right">Aa</NavItem>
+                    <NavSearch></NavSearch>
+                </Nav>
+                <Addition>
+                    <Button className="reg">注册</Button>
+                    <Button className="writting">写文章</Button>
+                </Addition>
             </HeaderWrapper>
+
         )
     }
 }
